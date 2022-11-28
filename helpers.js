3295fb608969dec9b49d0f530a0bd98cc54376bd
Jhon Useche <jhon.useche@telus.com>
2022-11-28 16:44:51 -0700

    Added a for loop to the generateRandomString function to loop through the string of numbers, lower and upper case letters.

--- helpers.js
+++ helpers.js
@@ -1,10 +1,21 @@
+/*
+*
+*/
 const generateRandomString = () => {
   const numbers =  '0123456789';
   const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
   const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
   const string = numbers + lowerLetters + upperLetters;
-  console.log(string);
+
+  let result = '';
+
+  for (const char of string) {
+    if (result.length >= 6) break;
+    const randomChar = string[Math.floor(Math.random() * string.length)];
+    result += randomChar;
+  }
+
+  console.log(result);
   return;
 };
 
