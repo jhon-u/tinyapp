82f62d1be7dd761ec6832c8e6a36de03f50f1bb3
Jhon Useche <jhon.useche@telus.com>
2022-11-28 17:05:15 -0700

    Updated the app.post code to assign the long URL from the form to the random generated string and add them tot he urlDatabase obj as key values

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {generateRandomString} = require('helpers');
+const {generateRandomString} = require('./helpers');
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
@@ -14,8 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // POST Routes
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const randomStr = generateRandomString();
+  urlDatabase[randomStr] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send(JSON.stringify(urlDatabase));
 });
 
 // GET Routes: Path to view all the shorten and long URLs
@@ -35,7 +37,6 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
