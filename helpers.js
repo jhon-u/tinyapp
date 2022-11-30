a679ae2a6003ad719c87d0676f82e7445320b5de
Jhon Useche <jhon.useche@telus.com>
2022-11-29 20:06:13 -0700

    Extra comments added

--- helpers.js
+++ helpers.js
@@ -1,13 +1,15 @@
 /**
- * Returns a string of 6 random alphanumeric characters.
+ * Returns a string of random alphanumeric characters from a given length.
+ * @param  {number} idLength
+ * @returns  {string} a 6 character longe alphanumeric string
  */
 const generateRandomString = () => {
-  const numbers =  '0123456789';
-  const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
-  const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers =  "0123456789";
+  const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+  const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const string = numbers + lowerLetters + upperLetters;
   
-  let result = '';
+  let result = "";
   for (let i = 0; i < 6; i++) {
     const randomChar = string[Math.floor(Math.random() * string.length)];
     result += randomChar;
